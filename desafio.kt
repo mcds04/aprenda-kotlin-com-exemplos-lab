4b184aa531dbe759c5d7008d1b7189681ae5a220
Cris Sousa <100251166+mcds04@users.noreply.github.com>
2024-01-23 19:16:47 -0300

    Ajustes Classe Usuario: Adicionei dois campos à classe Usuario, nome e nivel,Função matricular: Na função matricular, adicionei o usuário à lista de inscritos e imprimi uma mensagem de confirmação,Função main: No método main, criei alguns objetos Usuario e ConteudoEducacional, e usei a função matricular para matricular os usuários na formação.

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,6 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val nivel: Nivel)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +9,20 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado com sucesso na formação ${this.nome}.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João Vitor", Nivel.BASICO)
+    val usuario2 = Usuario("Maria Cristina", Nivel.INTERMEDIARIO)
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 60)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120)
+
+    val formacao = Formacao("Formação em Kotlin", listOf(conteudo1, conteudo2))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
 }
